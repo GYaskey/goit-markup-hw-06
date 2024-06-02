438c329266fd30bdb283707f433d18f21a17eb9d
Gleb Yasynskyi <91141358+Gleb-Yas@users.noreply.github.com>
2024-06-02 17:49:26 +0300

    add mobile menu, add js for menu and modal, add mobile and tablet adaptability

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const menuBtns = document.querySelectorAll('.js-menu-btn');
+const menuEl = document.querySelector('.menu-container')
+
+
+menuBtns.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        menuEl.classList.toggle('is-open');
+
+    });
+});
+
